9f575a0dfd2e69f918419ca60edb2bb34bc899a5
H.M. Arslan <hmarslan234@gmail.com>
2023-07-22 17:53:00 +0500

    Initial commit

---
+++ project.py
@@ -0,0 +1,112 @@
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import cv2
+import urllib.request
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+# # Define data generators
+# train_dir = 'data/train'
+# val_dir = 'data/test'
+
+# num_train = 22841
+# num_val = 5704
+# batch_size = 64
+# num_epoch = 50
+
+# train_datagen = ImageDataGenerator(rescale=1./255)
+# val_datagen = ImageDataGenerator(rescale=1./255)
+
+# train_generator = train_datagen.flow_from_directory(
+#         train_dir,
+#         target_size=(48,48),
+#         batch_size=batch_size,
+#         color_mode="grayscale",
+#         class_mode='categorical')
+
+# validation_generator = val_datagen.flow_from_directory(
+#         val_dir,
+#         target_size=(48,48),
+#         batch_size=batch_size,
+#         color_mode="grayscale",
+#         class_mode='categorical')
+
+# Create the model
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+## If you want to train the same model, go for this
+# model.compile(loss='categorical_crossentropy',optimizer=Adam(learning_rate=0.0001),metrics=['accuracy'])
+# model_info = model.fit(
+#        train_generator,
+#        steps_per_epoch=num_train // batch_size,
+#        epochs=num_epoch,
+#        validation_data=validation_generator,
+#        validation_steps=num_val // batch_size)
+# plot_model_history(model_info)
+# model.save_weights('model.h5')
+
+# emotions will be displayed on your face from the webcam feed
+model.load_weights('model.h5')
+
+# prevents openCL usage and unnecessary logging messages
+cv2.ocl.setUseOpenCL(False)
+
+# dictionary which assigns each label an emotion (alphabetical order)
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# start the webcam feed
+cap = cv2.VideoCapture(0)
+## start with wireless IP Camera
+#url='http://192.168.238.18/cam-hi.jpg'
+while True:
+        ## Wireless IP Camera ##
+        #img_resp=urllib.request.urlopen(url)
+        #imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
+        #frame=cv2.imdecode(imgnp,-1)
+
+    # Find haar cascade to draw bounding box around face with Device Webcam
+    ret, frame = cap.read()
+    if not ret:
+        break  
+    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('Video', cv2.resize(frame,(1000,700),interpolation = cv2.INTER_CUBIC))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
